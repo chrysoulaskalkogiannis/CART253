823f82f0e353ecf827da2c61151adf8e3513f737
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2019-10-23 14:16:50 -0400

    Ex6: fixed constructor spelling

--- Exercises/exercise6/js/Predator.js
+++ Exercises/exercise6/js/Predator.js
@@ -14,7 +14,7 @@ class Predator {
   //
   // Sets the initial values for the Predator's properties
   // Either sets default values or uses the arguments provided
-  construction(x, y, speed, fillColor, radius) { //////////fixed fillColor
+  constructor(x, y, speed, fillColor, radius) { //////////fixed fillColor ////////// fixed constructor spelling 
     // Position
     this.x = x;
     this.y = y;
