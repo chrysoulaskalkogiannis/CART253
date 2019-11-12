6c4b2bf870ac1a07d4fd9b72505be13a993a23c4
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2019-11-12 17:19:21 -0500

    Ex7: added player movement and maze function

--- Exercises/exercise7/js/script.js
+++ Exercises/exercise7/js/script.js
@@ -9,22 +9,16 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+let x = 100;
+let y = 100;
 
-// preload()
-//
-// Description of preload
+let vx = 0;
+let vy = 0;
 
-function preload() {
-
-}
-
-
-// setup()
-//
-// Description of setup
+let playerSpeed = 10;
 
 function setup() {
-
+createCanvas(500,500);
 }
 
 
@@ -33,5 +27,56 @@ function setup() {
 // Description of draw()
 
 function draw() {
+background(0);
+player();
+maze();
+
 
 }
+
+function player(){
+
+  push();
+  fill(255,0,0);
+  ellipse(x,y,100);
+  pop();
+
+    x += vx;
+    y += vy;
+
+  if (keyIsDown(LEFT_ARROW)) {
+vx = -playerSpeed;
+   }
+   else if (keyIsDown(RIGHT_ARROW)) {
+     vx = playerSpeed;
+   }
+   else {
+     vx = 0;
+   }
+   // Vertical movement
+   if (keyIsDown(UP_ARROW)) {
+     vy = -playerSpeed;
+   }
+   else if (keyIsDown(DOWN_ARROW)) {
+     vy = playerSpeed;
+   }
+   else {
+     vy = 0;
+   }
+ }
+
+
+
+function maze(){
+  push();
+fill(0,255,0);
+rect(200,200,25,50);
+pop();
+}
+
+
+
+
+// make small maze
+//impliment enemy class
+// enemy movement
