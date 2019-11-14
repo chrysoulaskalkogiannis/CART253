d0dd159487da92fe489da808c749b8f1a02babad
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2019-11-14 17:25:39 -0500

    Ex7: implimented player movement and display

--- Exercises/exercise7/js/Player.js
+++ Exercises/exercise7/js/Player.js
@@ -1,5 +1,49 @@
 class Player {
-  constructor() {
-    
+
+
+  constructor(x, y, size, speed, health) {
+    this.x = x;
+    this.y = x;
+
+    this.size = size;
+
+    this.vx = 0;
+    this.vy = 0;
+
+    this.speed = speed;
+
+    this.health = health;
+  }
+
+  movement(){
+    this.x += this.vx;
+    this.y += this.vy;
+
+    if (keyIsDown(LEFT_ARROW)) {
+  this.vx = -this.speed;
+     }
+     else if (keyIsDown(RIGHT_ARROW)) {
+       this.vx = this.speed;
+     }
+     else {
+       this.vx = 0;
+     }
+     // Vertical movement
+     if (keyIsDown(UP_ARROW)) {
+       this.vy = -this.speed;
+     }
+     else if (keyIsDown(DOWN_ARROW)) {
+       this.vy = this.speed;
+     }
+     else {
+       this.vy = 0;
+     }
+  }
+
+  display(){
+    push();
+    fill(255,0,0);
+    ellipse(this.x,this.y,100);
+    pop();
   }
 }
--- Exercises/exercise7/js/script.js
+++ Exercises/exercise7/js/script.js
@@ -9,16 +9,13 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
-let x = 100;
-let y = 100;
 
-let vx = 0;
-let vy = 0;
 
-let playerSpeed = 10;
+let bob;
 
 function setup() {
 createCanvas(500,500);
+bob = new Player(100, 100, 60, 10, 100);
 }
 
 
@@ -28,55 +25,8 @@ createCanvas(500,500);
 
 function draw() {
 background(0);
-player();
-maze();
 
+bob.movement();
+bob.display();
 
 }
-
-function player(){
-
-  push();
-  fill(255,0,0);
-  ellipse(x,y,100);
-  pop();
-
-    x += vx;
-    y += vy;
-
-  if (keyIsDown(LEFT_ARROW)) {
-vx = -playerSpeed;
-   }
-   else if (keyIsDown(RIGHT_ARROW)) {
-     vx = playerSpeed;
-   }
-   else {
-     vx = 0;
-   }
-   // Vertical movement
-   if (keyIsDown(UP_ARROW)) {
-     vy = -playerSpeed;
-   }
-   else if (keyIsDown(DOWN_ARROW)) {
-     vy = playerSpeed;
-   }
-   else {
-     vy = 0;
-   }
- }
-
-
-
-function maze(){
-  push();
-fill(0,255,0);
-rect(200,200,25,50);
-pop();
-}
-
-
-
-
-// make small maze
-//impliment enemy class
-// enemy movement
