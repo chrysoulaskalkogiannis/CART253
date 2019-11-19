7ebf86668b5dbcdf3aa9acfd53395e2abb8ed5d5
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2019-11-19 15:26:02 -0500

    Ex8: working on walls

---
+++ Exercises/exercise8/js/Wall.js
@@ -0,0 +1,23 @@
+class Wall {
+  constructor(x,y,size) {
+    this.x;
+    this.y;
+
+    this.size;
+  }
+
+
+
+
+
+  display(){
+    push();
+    fill(0,0,255);
+    rect(this.x, thix.y, this.size);
+
+    pop();
+
+
+  }
+
+}
