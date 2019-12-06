859831640d42eacc6de09ce26cb1e5531a37ae63
chrysoulaskalkogiannis <44040918+chrysoulaskalkogiannis@users.noreply.github.com>
2019-12-06 00:41:29 -0500

    Pro3: array working for ore, working on wall

--- Projects/Project3/js/Ore.js
+++ Projects/Project3/js/Ore.js
@@ -1,17 +1,17 @@
 class Ore {
-  constructor(size) {
-    this.x = random(0, 900);
-    this.y = random(0, 825);
+  constructor(x, y, size, image) {
+    this.x = x;
+    this.y = y;
 
     this.size = size;
+    this.image = image;
   }
 
   // displays the ore as a small pink rectangle
   display() {
     push();
     fill(255, 60, 100);
-    rect(this.x, this.y, this.size * 2, this.size * 2);
-
+  image(this.image, this.x, this.y, this.size * 2, this.size * 2);
     pop();
   }
 
--- Projects/Project3/js/script.js
+++ Projects/Project3/js/script.js
@@ -24,13 +24,54 @@ let badGuy;
 
 let gameStart = false;
 
+// the array for the ore
+let numOre = 4
+let ore = []
+
+// the array for the walls
+let numWalls = 2;
+let wallsArray = []
+
+//images
+let goldOre;
+
+
+function preload() {
+  // load images
+
+  goldOre = loadImage("assets/images/goldOre.png");
+
+
+}
+
 function setup() {
   createCanvas(1000, 900);
   bob = new Player(100, 100, 50, 10);
   badGuy = new Enemy(500, 100, 10, 50);
   healthKit = new HealthPack(200, 200, 50);
   walls = new Wall(500, 500, 100);
-  gold = new Ore(25);
+  gold = new Ore(100, 100, 25, goldOre);
+
+
+//array for the ore
+for (let i = 0; i < numOre; i++){
+      let oreX = random(0, width);
+      let oreY = random(0, height);
+      let oreSize = random([10, 15, 20, 25]);
+   ore.push(new Ore(oreX, oreY, oreSize, goldOre));
+}
+
+   for (let i = 0; i < numWalls; i++){
+         let wallsArrayX = random(0, width);
+         let wallsArrayY = random(0, height);
+         let wallsArraySize = random([100, 125, 150,200]);
+      wallsArray.push(new Wall(wallsArrayX, wallsArrayY, wallsArraySize));
+
+
+
+}
+
+
 }
 
 // draw()
@@ -62,6 +103,20 @@ function draw() {
     badGuy.display();
     badGuy.handlePickUp(gold);
     badGuy.handleEating(bob)
+
+
+// display the ore array
+    for (let i = 0; i < ore.length; i++) {
+          ore[i].display();
+          bob.handlePickUp(ore[i]);
+        }
+
+        for (let i = 0; i < wallsArray.length; i++) {
+              wallsArray[i].display();
+              wallsArray[i].handleSpeed(bob);
+            }
+
+
   }
 
 
